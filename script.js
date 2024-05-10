6b268ceaa3103356e204a98e39d72ff7c31244d3
AishwrayaShimpi <aishwaryashimpi2001@gmail.com>
2024-05-10 15:59:23 +0530

    added lotie animation

--- script.js
+++ script.js
@@ -7,10 +7,61 @@ function toggleMenu() {
 
   
   var animation = bodymovin.loadAnimation({
-    container: document.getElementById('lottie-container'),
+    container: document.getElementById('lottie-container1'),
     renderer: 'svg', // Change this to 'canvas' if you prefer
     loop: true,
     autoplay: true,
     path: './Lottie/girlwithcomputer.json' // Path to your Lottie animation JSON file
   });
+
+
+  // Load the animation
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-container2'),
+    renderer: 'svg', // Change this to 'canvas' if you prefer
+    loop: true,
+    autoplay: true,
+    path: './Lottie/taskpro.json' // Path to your Lottie animation JSON file
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-container3'),
+    renderer: 'svg', // Change this to 'canvas' if you prefer
+    loop: true,
+    autoplay: true,
+    path: './Lottie/movies.json' // Path to your Lottie animation JSON file
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-container4'),
+    renderer: 'svg', // Change this to 'canvas' if you prefer
+    loop: true,
+    autoplay: true,
+    path: './Lottie/shoe.json' // Path to your Lottie animation JSON file
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-container5'),
+    renderer: 'svg', // Change this to 'canvas' if you prefer
+    loop: true,
+    autoplay: true,
+    path: './Lottie/ochi.json' // Path to your Lottie animation JSON file
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-container6'),
+    renderer: 'svg', // Change this to 'canvas' if you prefer
+    loop: true,
+    autoplay: true,
+    path: './Lottie/rayban.json' // Path to your Lottie animation JSON file
+  });
+
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('lottie-container7'),
+    renderer: 'svg', // Change this to 'canvas' if you prefer
+    loop: true,
+    autoplay: true,
+    path: './Lottie/Synergy.json' // Path to your Lottie animation JSON file
+  });
+
 
